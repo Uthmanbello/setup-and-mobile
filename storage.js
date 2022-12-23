5987927aa92138d6e2b99fb3bb1cbcee4e3305cc
Uthmanbello <uthmanbello88@gmail.com>
2022-12-23 21:43:25 +0100

    Add the storage part of the storage.js file

--- storage.js
+++ storage.js
@@ -1,13 +1,30 @@
 const formEntries = JSON.parse(localStorage.getItem('contactForm')) || {
-    name: '',
-    email: '',
-    message: '',
-  };
-  
-  const saveInputValues = () => {
-    name.value = formEntries.name;
-    email.value = formEntries.email;
-    message.value = formEntries.message;
-  };
+  name: '',
+  email: '',
+  message: '',
+};
 
-  
+const saveInputValues = () => {
+  name.value = formEntries.name;
+  email.value = formEntries.email;
+  message.value = formEntries.message;
+};
+
+saveInputValues();
+
+const LocalStorage = () => {
+  localStorage.setItem('contactForm', JSON.stringify(formEntries));
+};
+
+name.addEventListener('input', () => {
+  formEntries.name = name.value;
+  LocalStorage();
+});
+emailid.addEventListener('input', () => {
+  formEntries.email = email.value;
+  LocalStorage();
+});
+message.addEventListener('input', () => {
+  formEntries.message = message.value;
+  LocalStorage();
+});
